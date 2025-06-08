fef3b6963e798c9df4ceb634d5ed78df55552c94
Ricardo55010 <richi_mari.2011@hotmail.com>
2025-06-08 15:22:48 -0600

    fix: corrected wrong use of reference for properties of a struct. I was accessing property of a data with -> when it no longer was a reference thanks to *

--- consult/main.c
+++ consult/main.c
@@ -8,17 +8,37 @@ typedef struct{
 } Person;
 int main(){
     int option = 0;
-    Person * p = calloc(1,sizeof(Person));
+    int maxPersons = 1;
+    Person * p = calloc(maxPersons,sizeof(Person));
+    int currentPersons = 0;
+    char name[20];
+    
 
     do{
         printMenu();
         getOption(&option);
         if(option == 1){
+            if(currentPersons == maxPersons){
+                maxPersons *= 2;
+                p = realloc(p,maxPersons*sizeof(Person));
+            }
            printf("Register the name:"); 
-           scanf("%s",p->name);
-           printf("The name %s was registered\n",p->name);
+           scanf("%s",&((p+currentPersons)->name));
+           printf("The name %s was registered\n",(p+currentPersons)->name);
+           currentPersons++;
+           printf("current persons: %d\n",currentPersons);
+           printf("max persons: %d\n",maxPersons);
         }
-    }while(option>3 || option <1);
+        if(option == 2){
+            printf("Looking for the name: ");
+            for(int i = 0; i < currentPersons; i++){
+                printf("Person number %i %s\n",i,p[i].name);
+            }
+        }
+        if(option == 3){
+            printf("Goodbye\n");
+        }
+    }while(option!=3 && (option > 0 && option < 2));
     system("pause");
     return 0;
 }
